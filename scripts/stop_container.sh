ea0a78587d989a4fc1e08b636babed38cc7b7430
GVP <59083997+pavankalyan524@users.noreply.github.com>
2025-02-25 21:42:42 +0530

    Updated-3-stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,11 +2,4 @@
 set -e
 
 # Stop the running container (if any)
-container_id=`docker ps | awk 'NR>1 {print $1}'`  # Skip header and get container ID
-
-if [[ ! -z "$container_id" ]]; then
-  docker rm -f $container_id
-  echo "Stopped and removed container: $container_id"
-else
-  echo "No running container found."
-fi
+echo "Hii"
